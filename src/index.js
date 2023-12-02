0b07dd1a42b5f72f54afef886d7616c0294a257e
andrewbal93 <131342609+andrewbal93@users.noreply.github.com>
2023-12-02 21:00:59 +0200

    Profile Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import Profile from 'components/Profile/Profile';
+import user from './data/user.json';
 import './index.css';
+import './components/Profile/Profile.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile {...user} />
   </React.StrictMode>
 );

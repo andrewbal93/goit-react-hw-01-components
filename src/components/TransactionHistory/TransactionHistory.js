641d0c726521dbba50cd6beb25568e10f1e7a20d
andrewbal93 <131342609+andrewbal93@users.noreply.github.com>
2023-12-03 18:32:21 +0200

    Transactions_component

---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,24 @@
+import Transaction from './Transaction';
+
+export default function TransactionHistory({ transactions }) {
+  return (
+    <div>
+      <table className="transaction-history">
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {transactions.map(item => (
+            <Transaction key={item.id} {...item} />
+          ))}
+        </tbody>
+      </table>
+      ;
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statisics/Statistics';
 import Friends from 'components/Friends/Friends';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 import user from './data/user.json';
 import stat from './data/stat.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 import './index.css';
 import './components/Profile/Profile.css';
 import './components/Statisics/statistic.css';
 import './components/Friends/friends.css';
+import './components/TransactionHistory/transactions.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -17,5 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Statistics title="Upload stats" stats={stat} />
     <Statistics stats={stat} />
     <Friends friends={friends} />
+    <TransactionHistory transactions={transactions} />
   </React.StrictMode>
 );
